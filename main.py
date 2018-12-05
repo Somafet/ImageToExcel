e492f65e6b7bacb439589a5f78517d70695e22da
Soma Somorjai <sam@power-planner.com>
2018-12-05 00:11:41 +0000

    Final commit

--- main.py
+++ main.py
@@ -32,15 +32,13 @@ alpha = len(im_pixels[0, 0]) == 4 # check if alpha channel is included.
 
 print('Number of cells: ', str(im_size[0] * im_size[1]))
 
-
-
 workbook = xlsxwriter.Workbook(f'output/{file_name}.xlsx', {'constant_memory': True})
 # workbook = xlsxwriter.Workbook(f'output/{file_name}.xlsx')
 ws = workbook.add_worksheet('Image')
 ws.set_default_row(10)
 
-
 formats = {}
+
 for row in range(0, im_size[0]):
     for col in range(0, im_size[1]):
         if not alpha:
@@ -55,5 +53,7 @@ for row in range(0, im_size[0]):
 
         write_cell(ws, row, col, formats[color])
         ws.set_column(col, col, 1)
+
 print('Colours used: ', len(formats.keys()))
+
 workbook.close()
